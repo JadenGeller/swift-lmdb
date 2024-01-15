dfc128e5286377a9317dfe28dc86f0499b9bafcf
Jaden Geller <jaden.geller@gmail.com>
2024-01-14 16:09:07 -0800

    Add logging

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -5440,6 +5440,7 @@ mdb_env_setup_locks(MDB_env *env, MDB_name *fname, int mode, int *excl)
 		 */
 		sem_unlink(MUTEXNAME(env, 'r'));
 		sem_unlink(MUTEXNAME(env, 'w'));
+        fprintf(stderr, "MUTEXNAME: '%s'\n", MUTEXNAME(env, 'r'));
 		env->me_rmutex = sem_open(MUTEXNAME(env, 'r'), O_CREAT|O_EXCL, mode, 1);
 		if (env->me_rmutex == SEM_FAILED) goto fail_errno;
 		env->me_wmutex = sem_open(MUTEXNAME(env, 'w'), O_CREAT|O_EXCL, mode, 1);
