e237c19ccc868909c52f88f87fd80728705dc8bc
Jaden Geller <jaden.geller@gmail.com>
2024-01-14 16:59:37 -0800

    WHATEV

--- Package.swift
+++ Package.swift
@@ -4,11 +4,9 @@ import PackageDescription
 import Foundation
 
 func foo() -> [CSetting] {
-    let fileURL = FileManager.default.homeDirectoryForCurrentUser.appending(component: "Downloads/out_test.txt")
-    try! JSONEncoder().encode(ProcessInfo.processInfo.environment).write(to: fileURL)
     return (ProcessInfo.processInfo.environment["APPGROUP_ID"].map {
         [.define("MDB_POSIX_SEM_PREFIX", to: $0)]
-    } ?? [])
+    } ?? [.define("MDB_POSIX_SEM_PREFIX", to: "\"BLAH\"")])
 }
 
 let package = Package(
