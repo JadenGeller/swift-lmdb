8333420ba60979cea632d2b0bc9d9e1455345879
Jaden Geller <jaden.geller@gmail.com>
2024-01-14 18:28:18 -0800

    TEST

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             dependencies: [],
             cSettings: [
                 .define("MDB_USE_POSIX_SEM")
-            ] + foo()
+            ] //+ foo()
         )
     ]
 )
